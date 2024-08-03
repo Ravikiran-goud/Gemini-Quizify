a7e4999869aa7e5418552e3fb2a2089dbd989024
Ravi Kiran Goud Jaggari <122633050+Ravikiran-goud@users.noreply.github.com>
2024-08-02 23:04:59 -0400

    first commit

---
+++ tasks/task_6/task_6.py
@@ -0,0 +1,53 @@
+import sys
+import os
+import streamlit as st
+sys.path.append(os.path.abspath('../../'))
+from tasks.task_3.task_3 import DocumentProcessor
+from tasks.task_4.task_4 import EmbeddingClient
+from tasks.task_5.task_5 import ChromaCollectionCreator
+
+if __name__ == "__main__":
+    st.header("Quizzify")
+
+    # Configuration for EmbeddingClient
+    embed_config = {
+        "model_name": "textembedding-gecko@003",
+        "project": "gemini-quizify-430616",
+        "location": "us-central1"
+    }
+    
+    screen = st.empty() # Screen 1, ingest documents
+    with screen.container():
+        st.header("Quizzify")
+
+        # 1) Initialize DocumentProcessor and Ingest Documents from Task 3
+        processor = DocumentProcessor()  
+        processor.ingest_documents()     
+
+        # 2) Initialize the EmbeddingClient from Task 4 with embed config
+        embed_client = EmbeddingClient(**embed_config)  
+
+        # 3) Initialize the ChromaCollectionCreator from Task 5
+        chroma_creator = ChromaCollectionCreator(processor, embed_client)
+
+        with st.form("Load Data to Chroma"):
+            st.subheader("Quiz Builder")
+            st.write("Select PDFs for Ingestion, the topic for the quiz, and click Generate!")
+
+            # 4) Use streamlit widgets to capture the user's input
+            topic_input = st.text_input("Enter the topic for the quiz")
+            num_questions = st.slider("Select the number of questions", 1, 10, 5)
+
+            submitted = st.form_submit_button("Generate a Quiz!")
+            if submitted:
+                # 5) Use the create_chroma_collection() method to create a Chroma collection from the processed documents
+                chroma_creator.create_chroma_collection()
+
+                # Uncomment the following lines to test the query_chroma_collection() method
+        document = chroma_creator.query_chroma_collection(topic_input) 
+
+    if document:
+        screen.empty() # Screen 2
+        with st.container():
+            st.header("Query Chroma for Topic, top Document: ")
+            st.write(document)
---
+++ tasks/task_9/task_9.py
@@ -0,0 +1,109 @@
+import streamlit as st
+import os
+import sys
+import json
+sys.path.append(os.path.abspath('../../'))
+from tasks.task_3.task_3 import DocumentProcessor
+from tasks.task_4.task_4 import EmbeddingClient
+from tasks.task_5.task_5 import ChromaCollectionCreator
+from tasks.task_8.task_8 import QuizGenerator
+
+class QuizManager:
+    def __init__(self, questions: list):
+        """
+        Initialize the QuizManager class with a list of quiz questions.
+        """
+        self.questions = questions
+        self.total_questions = len(questions)
+
+    def get_question_at_index(self, index: int):
+        """
+        Retrieves the quiz question object at the specified index.
+        """
+        # Ensure index is always within bounds using modulo arithmetic
+        valid_index = index % self.total_questions
+        return self.questions[valid_index]
+    
+    def next_question_index(self, direction=1):
+        """
+        Adjust the current quiz question index based on the specified direction.
+        """
+        if "question_index" not in st.session_state:
+            st.session_state["question_index"] = 0
+
+        current_index = st.session_state["question_index"]
+        new_index = (current_index + direction) % self.total_questions
+        st.session_state["question_index"] = new_index
+
+# Test Generating the Quiz
+if __name__ == "__main__":
+    
+    embed_config = {
+        "model_name": "textembedding-gecko@003",
+        "project": "gemini-quizify-430616",
+        "location": "us-central1"
+    }
+    
+    screen = st.empty()
+    with screen.container():
+        st.header("Quiz Builder")
+        processor = DocumentProcessor()
+        processor.ingest_documents()
+    
+        embed_client = EmbeddingClient(**embed_config) 
+    
+        chroma_creator = ChromaCollectionCreator(processor, embed_client)
+    
+        question = None
+        question_bank = None
+    
+        with st.form("Load Data to Chroma"):
+            st.subheader("Quiz Builder")
+            st.write("Select PDFs for Ingestion, the topic for the quiz, and click Generate!")
+            
+            topic_input = st.text_input("Topic for Generative Quiz", placeholder="Enter the topic of the document")
+            questions = st.slider("Number of Questions", min_value=1, max_value=10, value=1)
+            
+            submitted = st.form_submit_button("Submit")
+            if submitted:
+                chroma_creator.create_chroma_collection()
+                
+                st.write(topic_input)
+                
+                # Test the Quiz Generator
+                generator = QuizGenerator(topic_input, questions, chroma_creator)
+                question_bank = generator.generate_quiz()
+
+    if question_bank:
+        screen.empty()
+        with st.container():
+            st.header("Generated Quiz Question: ")
+            
+            # Initialize the QuizManager with the generated questions
+            quiz_manager = QuizManager(question_bank)
+            
+            # Format the question and display
+            with st.form("Multiple Choice Question"):
+                index_question = quiz_manager.get_question_at_index(0)  # Use the get_question_at_index method to set the 0th index
+                
+                # Unpack choices for radio
+                choices = []
+                for choice in index_question['choices']:  # For loop to unpack the data structure
+                    key = choice['key']
+                    value = choice['value']
+                    choices.append(f"{key}) {value}")
+                
+                st.write(index_question['question'])  # Display the question onto Streamlit
+                
+                answer = st.radio(  # Display the radio button with the choices
+                    'Choose the correct answer',
+                    choices
+                )
+                form_submitted = st.form_submit_button("Submit")
+                
+                if form_submitted:  # On click submit
+                    correct_answer_key = index_question['answer']
+                    if answer.startswith(correct_answer_key):  # Check if the answer is correct
+                        st.success("Correct!")
+                    else:
+                        st.error("Incorrect!")
